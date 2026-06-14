93c67625867eee789b74a0329b71371810c4c325
Arshia <arshiamohammadzadeh355@gmail.com>
2026-06-14 18:40:32 +0330

    feat(function &file): adding ignore file for ignore exe files. complite some two function Sign In/Up.

--- Cpp/Library_Manager.cpp
+++ Cpp/Library_Manager.cpp
@@ -29,6 +29,10 @@ protected:
     string Password;
     bool give = false;
     Book book;
+    /*Admin user & pass*/
+    string AdminUserName [2] = {"Arshia85","AMZ"};
+    string AdminPassword [2] = {"Arshia85","AMZ"};
+
 public:
     void SignIn(string UserName,string Password,string Name);
     void SignUp(string UserName,string Password);
@@ -37,7 +41,30 @@ public:
     void Return(bool give);
 };
 
+void User::SignIn(string Name,string UserName,string Password)
+{
+    this->UserName = UserName;
+    this->Password = Password;
+    this->Name = Name;
+    /*adding to the db files */
+}
 
+void User::SignUp(string UserName,string Password)
+{
+    for(int i = 0 ; i < 2 ; i++)
+    {
+        if (UserName == this->AdminUserName[i] && Password == this->AdminPassword[i])
+        {
+            cout<<"Welcom"<<AdminUserName[i];
+        }
+        /*search in db*/
+        else
+        {
+            cout<<"Not Found";
+        }
+    }
+    
+}
 class Admin : public User
 {
 public:
@@ -47,3 +74,14 @@ public:
 
 
 
+int main()
+{
+    Admin user;
+    string UserName ,Password;
+    cin>>UserName;
+    cin>>Password;
+    user.SignUp(UserName,Password);
+    
+    
+    
+}
