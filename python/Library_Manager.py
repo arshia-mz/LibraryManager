1b2313f954b626fd4f4bf04fc6c2803c8a3dc2ee
Arshia <arshiamohammadzadeh355@gmail.com>
2026-07-09 18:07:27 +0330

    finsh the classes and admin pannel.

--- python/Library_Manager.py
+++ python/Library_Manager.py
@@ -2,9 +2,9 @@
 import random
 import sqlite3
 
-#Admins 
-AdminUserName = ['Arshia85','AMZ']
-AdminPassWord = ['Arshia85','AMZ']
+#Admins #you Change to every UserPass you Want
+AdminUserName = ['arshia','***']
+AdminPassWord = ['arshia','***']
 
 #id generator
 def IdGenerator():
@@ -50,7 +50,13 @@ class Book():
         """, (BookName,))
         BookData.commit()
     def returnBook(self,BookName):
-        pass
+        Bookcursor.execute("""
+            UPDATE book 
+            SET give = Ture, 
+                count = count + 1 
+            WHERE name = ? AND give = False
+        """, (BookName,))
+        BookData.commit()
 class User():
     def __init__(self):
         self.Name = ''
@@ -70,26 +76,27 @@ class User():
         Usercursor.execute("SELECT * FROM users WHERE username=? AND password=?", (UserName, Password))
         result = Usercursor.fetchone()
         if result:
-            print(f"Welcom {UserName}")
+            return True
         else:
-            print('This UserName or Password is Not Define')
+            return False
 
     def search(self,BookName):
         self.book.search(BookName)
-    def Get():
-        pass
-    def Return():
-        pass
+    def Get(self,BookName):
+        self.book.getBook(BookName)
+    def Return(self,BookName):
+        self.book.returnBook(BookName)
     def ShowAll(self):
         self.book.ShowAll()
 class Admin(User):
     def __init__(self):
         super().__init__()
     def SignIn(self,UserName,Password):
-        if AdminUserName == UserName and AdminUserName == Password:
-            print(f'Welcome {UserName}')
+        for i,j in zip(AdminUserName,AdminPassWord):
+            if i == UserName and j == Password:
+                return True
         else:
-            print('The password or user is wrong')
+            return False
     def AddBook(self,BookName,Type,Year):
         self.book.AddBook(BookName,Type,Year)
     def RemoveBook(self,BookName):
@@ -97,8 +104,39 @@ class Admin(User):
 
 
 def main():
-    pass
-
-
+    idandity = input('chose your role (Amin(A),User(U)): ')
+    if(idandity == 'A'):
+       
+        admin = Admin()
+        UserName = input('Enter UserName: ')
+        Password = input('Enter Password: ')
+        if admin.SignIn(UserName,Password):
+            number = 0
+            print('Welcom')
+            while number != 5:
+                
+                print('Pannel: ' \
+                '1)AddBook'\
+                '2)RemoveBook'\
+                '3)ShowAll'\
+                '4)Search'\
+                '5)exite'
+                )
+                number = int(input(""))
+                if number == 1:
+                    BookName = input("Enter BookName: ")
+                    Type = input("Enter Type: ")
+                    Year = int(input("Enter Year: "))
+                    admin.AddBook(BookName,Type,Year)
+                if number == 2:
+                    BookName = input("Enter BookName: ")
+                if number == 3:
+                    admin.ShowAll()
+                if number == 4:
+                    BookName = input("Enter BookName: ")
+                    admin.search(BookName)
+        else:
+            print ('UserName or Password is wrong')
+            
 if __name__ == "__main__":
     main()
